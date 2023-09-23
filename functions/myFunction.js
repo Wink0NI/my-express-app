7df27e2f628c815b2810386110156af7bff5f3af
Wink0NI <olivier2003nc@gmail.com>
2023-09-23 20:55:45 +1100

    ok

--- functions/myFunction.js
+++ functions/myFunction.js
@@ -1,15 +1,10 @@
-// functions/express.js
+// myFunction.js
 const express = require("express");
-const serverless = require("serverless-http");
-
 const app = express();
-const router = express.Router();
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from Express.js serverless function!" });
 });
-
-app.use(`/.netlify/functions/express`, router);
 
 module.exports.handler = async (event, context) => {
   return require("serverless-http")(app)(event, context);
